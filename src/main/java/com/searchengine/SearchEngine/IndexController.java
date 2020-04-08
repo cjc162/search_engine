8255539919191d2bba2316e1394e9c26cce92cf3
Corey <cjc162@pitt.edu>
2020-04-08 01:22:15 -0400

    Deleted unnecessary imports

--- src/main/java/com/searchengine/SearchEngine/IndexController.java
+++ src/main/java/com/searchengine/SearchEngine/IndexController.java
@@ -1,26 +1,12 @@
 package com.searchengine.SearchEngine;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.dataproc.Dataproc;
-import com.google.api.services.dataproc.model.HadoopJob;
-import com.google.api.services.dataproc.model.Job;
-import com.google.api.services.dataproc.model.JobPlacement;
-import com.google.api.services.dataproc.model.JobReference;
-import com.google.api.services.dataproc.model.SubmitJobRequest;
-import com.google.common.collect.ImmutableList;
 
 @Controller
 public class IndexController {
